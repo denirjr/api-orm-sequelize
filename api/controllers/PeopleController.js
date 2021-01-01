bb64222a9c8fe68f4e428b68fac80e7497aa7c0d
Denir <denir-junior19@live.com>
2021-01-01 14:47:09 -0300

    (master): initiate MVC

---
+++ api/controllers/PeopleController.js
@@ -0,0 +1,14 @@
+const database = require("../models");
+
+class PeopleController {
+  static async getAllPeople(req, res) {
+    try {
+      const allPeople = await database.Pessoas.findAll();
+      return res.status(200).json(allPeople);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+}
+
+module.exports = PeopleController();
